bba71d1256633cd06e55d060281cf7251212be50
mgmilton <matt.milton@gmail.com>
2018-04-28 15:33:06 -0600

    adds quickSort function

--- sorting_suite/quickSort.js
+++ sorting_suite/quickSort.js
@@ -1,5 +1,23 @@
 function quickSort(array) {
-  if (array.length === 1) return array;
+  if (array.length <= 1) {
+    return array;
+  } else {
+      var left = [];
+      var right = [];
+      var newArray = [];
+      var pivot = array.pop();
+      var length = array.length;
 
-  var
+      for (var i = 0; i < length; i++) {
+        if (array[i] <= pivot) {
+          left.push(array[i]);
+        } else {
+          right.push(array[i]);
+        }
+      }
+      return newArray.concat(quickSort(left), pivot, quickSort(right))
+    }
 }
+console.log(quickSort([9,1,6]))
+
+module.exports = quickSort
